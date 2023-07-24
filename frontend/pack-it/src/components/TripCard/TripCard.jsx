c6af8998c4ac936a0da9c0fc89eab6189cbf993c
Brianna Gaines <bgaines@cisco.com>
2023-07-24 09:28:05 -0700

    end of day friday

--- frontend/pack-it/src/components/TripCard/TripCard.jsx
+++ frontend/pack-it/src/components/TripCard/TripCard.jsx
@@ -3,25 +3,26 @@ import Card from 'react-bootstrap/Card';
 import { Link } from 'react-router-dom';
 
 export const TripCard = ({ trip }) => {
-  const { endDate, startDate, coverPhoto, id } = trip;
+  const { endDate, startDate, coverPhoto, id, title } = trip;
   console.log('trip card: ', id);
   return (
     <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={coverPhoto} />
+      <Card.Img variant="top" src={coverPhoto} style={{ maxHeight: '9rem' }}/>
       <Card.Body>
-        {/* <Card.Title>{title}</Card.Title> */}
+        <Card.Title style={{textAlign: "center"}}>{title}</Card.Title>
         <Card.Text>
-          {startDate} - {endDate}
+          Start date: {startDate} - End date: {endDate}
         </Card.Text>
-        {/* <Button variant="primary">Packing List</Button> */}
+        <div className='d-grid  mx-auto'>
         <Link
-          className="btn btn-primary btn-lg"
+          className="btn btn-outline-info btn-lg"
           role="button"
           to={`/${id}/packingList`}
           id={id}
         >
           Packing List
         </Link>
+        </div>
       </Card.Body>
     </Card>
   );
--- frontend/pack-it/src/views/AllTrips/AllTrips.jsx
+++ frontend/pack-it/src/views/AllTrips/AllTrips.jsx
@@ -1,14 +1,20 @@
 import { TripCard } from '../../components/TripCard/TripCard';
-
+import { Row, Col } from 'react-bootstrap';
 export const AllTrips = ({ trips }) => {
   console.log('trips is', trips);
-  return trips.map((oneTrip) => (
-    <TripCard
-      trip={oneTrip}
-      title={oneTrip.title}
-      startDate={oneTrip.startDate}
-      endDate={oneTrip.endDate}
-      coverPhoto={oneTrip.coverPhoto}
-    />
-  ));
+  return (
+    <Row style={{marginTop: "5rem"}}>
+      {trips.map((oneTrip) => (
+        <Col>
+          <TripCard
+            trip={oneTrip}
+            title={oneTrip.title}
+            startDate={oneTrip.startDate}
+            endDate={oneTrip.endDate}
+            coverPhoto={oneTrip.coverPhoto}
+          />
+        </Col>
+      ))}
+    </Row>
+  );
 };
